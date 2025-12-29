453262b0ae046535cd0af2f38557fef8c0d7610b
Maggie0513 <h6741957g@gmail.com>
2025-12-29 22:12:59 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,4 @@
-const firebaseConfig = {
+export const firebaseConfig = {
     apiKey: "AIzaSyDy1AbtRBqhWyZSEhAC6JR7QXMM3EV-C8c",
     authDomain: "south-korea-37267.firebaseapp.com", 
     projectId: "south-korea-37267", 
